f9a33527c5c3eb64af61d3dcd8614157a787f580
Tope Taiwo <topzyray@yahoo.com>
2024-11-13 08:43:01 +0100

    Initial commits

---
+++ Python-Week-1/user_input.py
@@ -0,0 +1,9 @@
+print("Week 1 assignment on Python Programming at PLP")
+
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("Where is your current location? ")
+
+print(f"Hello {name}. Your are {age} years old. You live at {location}.")
+
+
